176e4436a2740936b80c8acd3c86f8d29989e596
Jakplayz <31926815+Jakplayz@users.noreply.github.com>
2017-10-11 14:21:02 +0200

    new stuffs
    uppdated server, and package.json and a debug.log

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const server = express()
+
+server.use(express.static('public'))
+
+
+
+function greets () {
+    var greet = ["Hei", "Hallo", "hei hei"];
+    var random = greet[Math.floor((Math.random()*greet.length))];
+    return random;
+}
+
+server.get('/random/:id', function (req, res) {
+  res.send(greets() +" "+ req.params.id);
+
+})
+
+server.listen(8080, function () {
+  console.log(greets() + " christian")
+  console.log('server listening on port 8080!')
+})
